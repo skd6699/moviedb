3187aa6ed2cda3f86cf7a9f8e612c8a60e0ed8e9
skd6699 <ksai0509@gmail.com>
2019-03-16 20:09:17 +0530

    Module 1 Keyword and search movie,pages

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-var    express = require("express");
-var app = express();
+var express = require("express"),
+  bodyParser = require("body-parser"),
+request = require("request");
+var app = module.exports.app = express();
 var http = require('http');
-var server = http.createServer(app);
-
-
-app.set("view engine","ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
 app.use(express.static("public"));
-
+var server = http.createServer(app);
+server.setTimeout(10*60*1000);
 //MYSQL CONNECTION
 var mysql = require('mysql');
 var con = mysql.createConnection({
@@ -20,26 +21,55 @@ con.connect(function(err) {
   if (err) throw err;
   con.query("SELECT * FROM watched ", function (err, result, fields) {
     if (err) throw err;
-    console.log(fields);
+    // console.log(fields);
   });
 });
 
+app.get("/",function(req, res) {
+   res.render("header"); 
+});
 
+app.get("/results",function(req,res){
+    var query = req.query.search;
+    res.redirect('/results/1/'+query);
+});
+app.get("/results/:page/:search",function(req,res){
+    var query = req.params.search;
+    var page = req.params.page;
+    var url = "http://www.omdbapi.com/?s=" + query + "&apikey=133b8b1e&page="+page;
+    request(url,function(error,response,body){
+        if(!error && response.statusCode == 200)
+        {
+            var r = JSON.parse(body);
+            console.log(r);
+            res.render("result",{data:r,search:query});
+        }
+    });
+});
+app.get("/app/:iid",function(req,res){
+     var imdbid = req.params.iid;
+   // https://api.themoviedb.org/3/find/tt3896198?api_key=debc0368eb9aad6d905a7962423eafd6&language=en-US&external_source=imdb_id
+    
 
-//ROUTES
-app.get('/', function(req, res) {
-    res.render("header");
+    var url = "http://www.omdbapi.com/?i=" + imdbid + "&apikey=133b8b1e"; 
+    request(url,function(error,response,body){
+        if(!error && response.statusCode == 200)
+        {
+            var r = JSON.parse(body);
+            console.log(r);
+            res.send(r);
+            // res.render("result",{data:r});
+        }
+    });
 });
 
 
+http://www.omdbapi.com/?i=tt3896198&apikey=133b8b1e
 
-
-
-
-
-
-
-server.listen(3000, 'localhost');
-server.on('listening', function() {
-    console.log('Express server started on port %s at %s', server.address().port, server.address().address);
-});
+//SERVER
+server.listen(3000);
+console.log('movie app server started %s', server.address().port);
+//debc0368eb9aad6d905a7962423eafd6
+server.on('connection', function(socket) {
+  socket.setTimeout(600 * 60 * 1000); // now works perfectly...
+})
